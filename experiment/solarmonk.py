860a1f833f562ca36bc1ca8c85bf07b82bdca2fa
techpriest_steamdech <davidbaunach@gmail.com>
2023-07-10 12:59:40 -0500

    started a py script for learning

---
+++ experiment/solarmonk.py
@@ -0,0 +1,5 @@
+if __name__ == "__main__":
+    while True:
+        print("Solarmonk")
+        print("Alice and Cheshire Cat stumble upon a monastery of Benedictine monks as they walk through a forest clearing. The monks do not know the way to the garden, but they are fascinated by its description, and plan to build their own one day. After a pleasant meal, Alice and Cheshire Cat continue on their way, and the monks continue their life of prayer and sustainable living. How long will they survive?")
+        
